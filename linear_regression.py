f406cdb924870cf448c693788e4082b7b67ef3f1
E Bolton <eebmagic@gmail.com>
2021-12-05 17:32:05 -0500

    move plotting to its own file

--- linear_regression.py
+++ linear_regression.py
@@ -5,29 +5,8 @@ from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
 
 from data_loader import DataSet
+from plotting import plot_predictions
  
-def plot_predictions(plot_data: list, title: str='Linear Regression: Predicted vs Actual'):
-    x_test = plot_data[0]
-    y_test = plot_data[1]
-    y_preds = plot_data[2]
-    r2 = plot_data[3]
-
-    plt.figure(figsize=(6,6))
-    plt.scatter(y_test, y_preds, color='deepskyblue', alpha=0.8, linewidths=0)
-
-    m, b = np.polyfit(y_test, y_preds, 1)
-    plt.plot(y_test, (m*y_test + b), color='black')
-
-    axis_max = max([max(y_test), max(y_preds)]) + 0.5
-    axis_min = min([min(y_test), min(y_preds)]) - 0.5
-    plt.axis([axis_min, axis_max, axis_min, axis_max])
-
-    plt.suptitle(title, size=10)
-    plt.title(f'R\u00b2 = {round(r2, 8)}       Slope of Trendline = {round(m, 8)}', size=10)
-    plt.xlabel('Actual Normalized Price Per Sq. Ft')
-    plt.ylabel('Predicted Normalized Price Per Sq. Ft')
-    return
-
 def nloss(y, yh):
     '''
     Mean squared loss. 
@@ -70,6 +49,9 @@ r2 = r2_score(y_test, pred)
 print(f'R2: {r2}')
 
 # Plot predicitons
-plot_data = [x_test, y_test, pred, r2]
-plot_predictions(plot_data)
+plot_data = {
+    'test': y_test,
+    'pred': pred
+}
+plot_predictions(plot_data, title='Linear Regression: Predicted vs Actual', filename='linearreg')
 plt.show()
---
+++ plotting.py
@@ -0,0 +1,39 @@
+import numpy as np
+from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
+
+def plot_predictions(plot_data: dict,
+    title: str='DEFAULT TITLE: Predicted vs Actual', filename: str=None):
+    '''
+    Parameters:
+        plot_data: A dictionary with 'test' (the true price target),
+            and 'pred' (the prices predicted by model).
+
+        title: Title to be used for the plot.
+
+        filename: Name to be used for image file export. If None, then no file will save.
+    '''
+    y_test = plot_data['test']
+    y_preds = plot_data['pred']
+
+    r2 = r2_score(y_test, y_preds)
+
+    plt.figure(figsize=(6,6))
+    plt.scatter(y_test, y_preds, color='deepskyblue', alpha=0.8, linewidths=0)
+
+    m, b = np.polyfit(y_test, y_preds, 1)
+    plt.plot(y_test, (m*y_test + b), color='black')
+
+    axis_max = max([max(y_test), max(y_preds)]) + 0.5
+    axis_min = min([min(y_test), min(y_preds)]) - 0.5
+    plt.axis([axis_min, axis_max, axis_min, axis_max])
+
+    plt.suptitle(title, size=10)
+    plt.title(f'R\u00b2 = {round(r2, 8)}       Slope of Trendline = {round(m, 8)}', size=10)
+    plt.xlabel('Actual Normalized Price Per Sq. Ft')
+    plt.ylabel('Predicted Normalized Price Per Sq. Ft')
+
+    if filename:
+        plt.savefig(f'image_exports/{filename}.png', bbox_inches='tight')
+
+    return
