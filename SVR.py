cb527f457c8b665bc44e2d68230841bfa368c65a
carlocasas99 <carlocasas1818@gmail.com>
2021-12-05 21:36:08 -0500

    remove gridsearch from SVR

--- SVR.py
+++ SVR.py
@@ -14,9 +14,9 @@ y = ds.zillow.flatten()
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-parameters = [{'kernel': ['rbf'], 'gamma': [1e-4, 1e-3, 0.01, 0.1, 0.2, 0.5, 0.6, 0.9],'C': [1, 10, 100, 1000, 10000]}]
-svr = GridSearchCV(svm.SVR(epsilon = 0.01), parameters, cv = 10)
-regr = svr.fit(x_train, y_train)
+#parameters = [{'kernel': ['rbf'], 'gamma': [1e-4, 1e-3, 0.01, 0.1, 0.2, 0.5, 0.6, 0.9],'C': [1, 10, 100, 1000, 10000]}]
+#svr = GridSearchCV(svm.SVR(epsilon = 0.01), parameters, cv = 10)
+regr = svm.SVR(kernel="rbf", C=10000, gamma=0.0001).fit(x_train, y_train)
 
 y_pred = regr.predict(x_test)
 plot_data = {
